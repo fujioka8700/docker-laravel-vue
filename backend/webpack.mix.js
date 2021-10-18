3271e3344961f59179ade934339c4ee2c51b2202
fujioka8700 <fujioka8700@gmail.com>
2021-10-18 20:37:55 +0900

    [add] Vue Router までインストール済

--- backend/webpack.mix.js
+++ backend/webpack.mix.js
@@ -6,23 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .browserSync({
-        proxy: {
-            target: "http://127.0.0.1"
-        },
-        files: [
-            './resources/**/*',
-            './public/**/*'
-        ],
-        open: false,
-        reloadOnRestart: true
-    });
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css');
